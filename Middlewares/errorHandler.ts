31e7eded370a2e9c591751ffaf5e7387caf2d049
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-09 16:49:09 +0100

    update

--- Middlewares/errorHandler.ts
+++ Middlewares/errorHandler.ts
@@ -1,7 +1,20 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError, HttpCode } from "../Util/AppError";
 
-
 const devErrorHandler = (err: AppError, res: Response) => {
-    
-}
+  res.status(HttpCode.INTERNAL_SERVER_ERROR).json({
+    error: err,
+    message: err.message,
+    stack: err.stack,
+    status: err.httpCode,
+  });
+};
+
+export const erroHandler = (
+  err: AppError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  devErrorHandler(err, res);
+};
--- Util/AppError.ts
+++ Util/AppError.ts
@@ -1,5 +1,12 @@
 export enum HttpCode {
   OK = 200,
+  CREATED = 201,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  CONFLICT = 409,
+  INTERNAL_SERVER_ERROR = 500,
 }
 
 interface AppErrorArgs {
