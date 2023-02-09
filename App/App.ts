7a6de27ffca7cc2abcb1fe334472589634e54bf6
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-09 17:32:35 +0100

    update

--- App/App.ts
+++ App/App.ts
@@ -7,7 +7,22 @@ import exppress, {
 
 import cors from "cors";
 import morgan from "morgan";
+import { AppError, HttpCode } from "../Util/AppError";
+import { erroHandler } from "../Middlewares/errorHandler";
 
 export const appConfig = (app: Application) => {
-  app.use(exppress.json()).use(cors()).use(morgan("dev"));
+  app
+    .use(exppress.json())
+    .use(cors())
+    .use(morgan("dev"))
+    .use("*", (req: Request, res: Response, next: NextFunction) => {
+      next(
+        new AppError({
+          message: `This Routes ${req.originalUrl} does not exist`,
+          httpCode: HttpCode.NOT_FOUND,
+          isOperational: true,
+        })
+      );
+    })
+    .use(erroHandler);
 };
