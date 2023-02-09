006131261e449235680070103ae805d7408fa5a9
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-09 14:48:32 +0100

    update

--- Config/db.ts
+++ Config/db.ts
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
 
-export const config = async () => {
-  try {
+const url: string = "mongodb://localhost:27017/pratice";
 
-  } catch (error) {
-    
+export const config = async (): Promise<void> => {
+  try {
+    const connectToDB = await mongoose.connect(url);
+    console.log("connected on", connectToDB.connection.host);
+  } catch (error: any) {
+    console.log("An error Occured In DB", error.message);
+    process.exit(1);
   }
 };
--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import express, { Application } from "express";
 import { appConfig } from "./App/App";
+import { config } from "./Config/db";
 
 const port: number = 2001;
 
 const app: Application = express();
 appConfig(app);
+config();
 
 app.listen(port, () => {
   console.log("Done on", port);
