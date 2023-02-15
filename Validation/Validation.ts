772fcc649bef098d432a94e1add1fdf62c071e39
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-15 21:15:19 +0100

    update

--- Validation/Validation.ts
+++ Validation/Validation.ts
@@ -1,28 +1,8 @@
 import { NextFunction } from "express";
-import joi from "joi";
-import { AppError, HttpCode } from "../Util/AppError";
+import Joi from "joi";
 
-export const validate = (
-  schemaName: joi.ObjectSchema,
-  body: object,
-  next: NextFunction
-) => {
-  const value = schemaName.validate(body, {
-    allowUnknown: true,
-    abortEarly: false,
-    stripUnknown: true,
-  });
 
-  try {
-    value.error
-      ? next(
-          new AppError({
-            httpCode: HttpCode.UNPROCESSABLE_IDENTITY,
-            message: value.error.details[0].message,
-          })
-        )
-      : next();
-  } catch (error) {
-    console.log(error);
-  }
-};
+
+export const validate = (schemaName: Joi.ObjectSchema, body: object, next: NextFunction) => {
+    
+}
