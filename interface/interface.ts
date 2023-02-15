d3cfadb123a2bf1acc47e628faec608db40bb889
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-15 20:18:20 +0100

    update

--- interface/interface.ts
+++ interface/interface.ts
@@ -4,24 +4,3 @@ export interface IData {
   password: string;
 }
 
-export interface IShop {
-  name: string;
-  address: string;
-  products: IProduct[];
-}
-
-export interface IProduct {
-  name: string;
-  category: string;
-  price: number;
-  no_In_Stock: number;
-  shop: IShop;
-}
-
-export interface IProduct {
-  name: string;
-  category: string;
-  price: number;
-  no_In_Stock: number;
-  shop: IShop;
-}
