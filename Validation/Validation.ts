3d493abc34e4f0f530429eca82fb1bee168acb47
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-15 21:04:26 +0100

    update

---
+++ Validation/Validation.ts
@@ -0,0 +1,11 @@
+import { NextFunction } from "express";
+import joi from "joi";
+import { AppError, HttpCode } from "../Util/AppError";
+
+
+export const validate = (schemaName: joi.ObjectSchema, body: object, next: NextFunction) => {
+    const value  = schemaName.validate({body, {
+        allowUnknown: true,
+        
+    }})
+}
