b0bc55d932c6f13b07f317455b21d8ad40ed893a
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-16 11:45:28 +0100

    update

--- Middlewares/errorHandler.ts
+++ Middlewares/errorHandler.ts
@@ -5,7 +5,7 @@ const devErrorHandler = (err: AppError, res: Response) => {
   return res.status(err.httpCode).json({
     error: err,
     status: err.httpCode,
-    name: err.name,
+    message: err.message,
     stack: err.stack,
   });
 };
