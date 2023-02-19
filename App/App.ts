bae57c495ab675592b5f7eaea21fd33b5da90262
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-19 16:08:06 +0100

    update

--- App/App.ts
+++
@@ -1,8 +0,0 @@
-import express, { Application } from "express";
-
-const port: number = 2001;
-const app: Application = express();
-
-app.listen(port, () => {
-  console.log("Done on", port);
-});
--- index.ts
+++ index.ts
@@ -1,13 +1,8 @@
 import express, { Application } from "express";
-import { appConfig } from "./App/App";
-import { config } from "./Config/db";
 
-const app: Application = express();
 const port: number = 2001;
-
-appConfig(app);
-config();
+const app: Application = express();
 
 app.listen(port, () => {
-  console.log("Don on Port", port);
+  console.log("Done on", port);
 });
