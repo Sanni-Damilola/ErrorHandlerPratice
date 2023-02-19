fedb26d98a4d6020bcd3e8824aaa1f85d2cc3119
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-19 16:20:56 +0100

    update

--- Util/AppError.ts
+++ Util/AppError.ts
@@ -1,4 +1,4 @@
-export enum httpCode = {
+export enum httpCode {
   OK = 200,
   CREATED = 201,
   BAD_REQUEST = 400,
@@ -8,4 +8,16 @@ export enum httpCode = {
   NOT_FOUND = 404,
   UNPROCESSABLE_IDENTITY,
   INTERNAL_SERVER_ERROR = 500,
+}
+
+interface argsError {
+  name?: string;
+  isOperational?: boolean;
+  message: string;
+  HttpCode: httpCode;
+}
+
+
+export class AppError extends Error {
+  
 }
