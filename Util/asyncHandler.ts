67b0ef4def70c4462eedd9262e5964c787b0d48c
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-19 16:03:35 +0100

    update

--- Util/asyncHandler.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export const asynHandler = (fn: any) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-};
